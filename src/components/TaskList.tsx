063afe04e07f6c58ea1a1edacd83aa7c3a10c05a
luan <luan@localhost.localdomain>
2022-07-29 16:23:21 -0300

    Functions implemented and working

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -16,14 +16,35 @@ export function TaskList() {
 
   function handleCreateNewTask() {
     // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    if (newTaskTitle.trim().length === 0){
+      console.log('Title is empty, task will not be created.');
+    } else {
+      const newId = Math.floor((Math.random() * 100) + 1);
+      const newTask: Task = {id: newId, title: newTaskTitle, isComplete: false};
+      const newTasks: Task[] = tasks;
+      tasks.push(newTask)
+      setTasks(newTasks);
+      setNewTaskTitle('');
+    }
+    
   }
 
   function handleToggleTaskCompletion(id: number) {
     // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    const newTasks = tasks;
+    newTasks.forEach(element => {
+      if (element.id === id) {
+        element.isComplete = !(element.isComplete.valueOf());
+      }
+    });
+    setTasks(newTasks);
   }
 
   function handleRemoveTask(id: number) {
     // Remova uma task da listagem pelo ID
+    const newTasks = tasks.filter(item => item.id !== id);
+    setTasks(newTasks);
+    
   }
 
   return (
